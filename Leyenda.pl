57956f67d364627fc03fe020f79549c7d2417db9
HackerGolden <145525768+HackerGolden@users.noreply.github.com>
2023-09-20 01:48:33 -0400

    Actualizar Leyenda.pl

--- Leyenda.pl
+++ Leyenda.pl
@@ -1,3 +1,6 @@
-archivo 1 -> Padre,madre,hija,hijo
-archivo 2 -> Diagnostico de dengue
-archivo 3 -> Diagnostico de dengue hemorragico
+archivo 1 -> Padre,madre,hija y hijo.
+archivo 2 -> Diagnostico de gripe y dengue.
+archivo 3 -> Diagnostico de dengue hemorragico.
+archivo 4 -> Diagnostico de Diabetes Tipo II.
+Archivo 5 -> Diagnostico de Ciclon, Huracan y 
+Depresion Tropical.
