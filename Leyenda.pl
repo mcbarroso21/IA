bfe67ef83c44f7b34e7a11561c52073dd36d0724
Marco Barroso <122231961+mcbarroso21@users.noreply.github.com>
2023-10-06 23:41:09 -0400

    Leyenda.pl

--- Leyenda.pl
+++ Leyenda.pl
@@ -2,7 +2,8 @@ archivo 1 -> Padre,madre,hija y hijo.
 archivo 2 -> Diagnóstico de gripe y dengue.
 archivo 3 -> Diagnóstico de dengue hemorragico.
 archivo 4 -> Diagnóstico de Diabetes Tipo II.
-Archivo 5 -> Diagnóstico de Ciclon, Huracan y 
-Depresion Tropical.
+Archivo 5 -> Diagnóstico de Ciclon, Huracan
 archivo 6 -> Diagnóstico de pronóstico del tiempo.
 archivo 7 -> Diagnóstico de enfermedades.
+archivo 8 -> Deteccion de Enfermedades Sireg2
+archivo 9 -> Diagnostico de fallo de equipos tecnologicos Sireg2
